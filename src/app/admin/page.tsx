9b4babd50d594f3f3322be10977f68a237cf8b7e
Sergio Beleño <sergiosabh05@gmail.com>
2024-03-06 22:41:30 -0500

    first commit

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { Admindashboard } from "@/components/Admindashboard";
+
+export default function Home() {
+  return (
+    <Admindashboard/>
+  );
+}
---
+++ src/app/employee/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { Employeedashboard } from "@/components/Employeedashboard";
+
+export default function Home() {
+  return (
+    <Employeedashboard/>
+  );
+}
+
